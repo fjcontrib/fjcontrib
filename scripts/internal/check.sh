0b69f1b324c4fe5492063a9fc73ff5d55dcc3ce1
Gregory Soyez <gregory.soyez@cern.ch>
2013-01-31 09:54:58 +0000

    made sure that the ref file contains more than comments and empty lines
    git-svn-id: svn+ssh://phab.hepforge.org/source/fastjetsvn/contrib/trunk@37 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- scripts/internal/check.sh
+++ scripts/internal/check.sh
@@ -14,8 +14,15 @@ echo "Checking the output of '$1' using the input file '$2'"
 # check that all the necessary files are in place
 test -e ./$1 || { echo "the executable $1 cannot be found."; exit 1;}
 test -x ./$1 || { echo "$1 is not executable."; exit 1;}
-test -e ./$1.ref || { echo "the expected result $1.ref cannot be found."; exit 1;}
+test -e ./$1.ref || { echo "the expected output $1.ref cannot be found."; exit 1;}
 test -e ./$2 || { echo "the datafile $2 cannot be found."; exit 1;}
+
+# make sure that the result file is not empty (after removal of
+# comments and empty lines)
+[ -z "$(cat ./$1.ref | grep -v '^#' | grep -v '^$')" ] && {
+    echo "The expected output should contain more than comments and empty lines"
+    exit 1
+}
 
 # run the example
 ./$1 < $2 2>/dev/null | grep -v "^#" > $1.tmp_ref
@@ -23,7 +30,7 @@ test -e ./$2 || { echo "the datafile $2 cannot be found."; exit 1;}
 DIFF=`cat $1.ref | grep -v "^#" | diff $1.tmp_ref -`
 if [[ -n $DIFF ]]; then 
     cat $1.ref | grep -v "^#" | diff $1.tmp_ref - > $1.diff
-    echo "Results differ (diff available in $1.diff)"
+    echo "Outputs differ (diff available in $1.diff)"
     rm $1.tmp_ref
     exit 1
 fi

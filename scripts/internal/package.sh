b8fcf567ade70c1a8126091920686cba969093fc
Matteo Cacciari <matteo.cacciari@cern.ch>
2013-02-06 22:00:36 +0000

    updated package.sh to make it compatible with new place for utils scripts
    git-svn-id: svn+ssh://phab.hepforge.org/source/fastjetsvn/contrib/trunk@149 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- scripts/internal/package.sh
+++ scripts/internal/package.sh
@@ -12,16 +12,21 @@
 
 version=`cat VERSION`
 packagename=fjcontrib-$version
-files="AUTHORS COPYING Makefile.in README VERSION NEWS configure data scripts/internal/check.sh scripts/internal/install-sh"
+files="AUTHORS COPYING Makefile.in README VERSION NEWS configure data ChangeLog utils/check.sh utils/install-sh"
 # get contribs list from contribs.svn
 files="$files "$(cat contribs.svn | grep -v "^#" | grep -v "^$" | awk '{printf $1" "}')
 # get contribs list from configure (may include "unofficial" local contribs)
 #files="$files "`./configure --list`
 
+echo
+echo Making a distclean, so as to only package relevant files
+echo
+make distclean
+echo
 echo Creating version $version
 echo Including: $files
-
 tar --exclude=".svn"  --transform "s,^,$packagename/," -zcf $packagename.tar.gz $files
+
 exit
 
 

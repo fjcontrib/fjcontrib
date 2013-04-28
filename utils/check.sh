778aa8db03fa639ecac9ec530dc710948467532d
Matteo Cacciari <matteo.cacciari@cern.ch>
2013-04-28 19:08:22 +0000

    fixed some typos in utils/checks.sh
    git-svn-id: svn+ssh://phab.hepforge.org/source/fastjetsvn/contrib/trunk@230 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- utils/check.sh
+++ utils/check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # usage:
-#   check <program> <datafile>
+#   ./check.sh <program> <datafile>
 #
-# this script checks if "<program>" exisits, run it using <datafile>
-# as an inpu and compares the output with "<program>.ref" (it fails if
+# this script checks if "<program>" exists, runs it using <datafile>
+# as an input, and compares the output with "<program>.ref" (it fails if
 # "<program>.ref" does not exist),
 #
 # Note that <program> is actually run using "./<program> < <datafile>"
@@ -20,7 +20,7 @@ test -x ./$1 || { echo "ERROR: $1 is not executable."; exit 1;}
 test -e ./$1.ref || { echo "ERROR: the expected output $1.ref cannot be found."; exit 1;}
 test -e ./$2 || { echo "ERROR: the datafile $2 cannot be found."; exit 1;}
 
-# make sure that the result file is not empty (after removal of
+# make sure that the reference output file is not empty (after removal of
 # comments and empty lines)
 [ -z "$(cat ./$1.ref | grep -v '^#' | grep -v '^$')" ] && {
     echo "ERROR: the reference output, $1.ref"

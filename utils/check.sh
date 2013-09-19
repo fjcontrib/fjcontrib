ec0472c9ecbba0e6b23365d0b88fa29350996fcf
Gregory Soyez <gregory.soyez@cern.ch>
2013-09-19 10:34:40 +0000

    have make check print a summary of failures and successes at the end instead of stopping at the first error
    git-svn-id: svn+ssh://phab.hepforge.org/source/fastjetsvn/contrib/trunk@327 3dcf5ceb-c996-4387-9042-f4b6870d0cfc

--- utils/check.sh
+++ utils/check.sh
@@ -9,16 +9,28 @@
 #
 # Note that <program> is actually run using "./<program> < <datafile>"
 
+function print_status_and_exit {
+    printf "  %-20s %-25s %s\n" `pwd | sed 's/.*\///g'` "$1" "$2" >> ../test_summary.tmp
+    #-----------------------------------------------------------------
+    # NOTE:
+    #   This will cause the check test to exit with no error. 
+    #   Comment this line out if you want errors to cause Make to stop
+    exit
+}
+
 echo
 echo -n "In directory" `pwd | sed 's/.*\///'`
 echo "/ checking the output of '$1' using the input file"
 echo "$2 and reference file $1.ref"
 
 # check that all the necessary files are in place
-test -e ./$1 || { echo "ERROR: the executable $1 cannot be found."; exit 1;}
-test -x ./$1 || { echo "ERROR: $1 is not executable."; exit 1;}
-test -e ./$1.ref || { echo "ERROR: the expected output $1.ref cannot be found."; exit 1;}
-test -e ./$2 || { echo "ERROR: the datafile $2 cannot be found."; exit 1;}
+#
+# Note that, for the executable, the Makefile should in principle
+# rebuild the program if it is absent or not executable
+test -e ./$1 || { echo "ERROR: the executable $1 cannot be found."; print_status_and_exit "$1" "Failed (executable not found)"; exit 1;}
+test -x ./$1 || { echo "ERROR: $1 is not executable."; print_status_and_exit "$1" "Failed (program not executable)"; exit 1;}
+test -e ./$1.ref || { echo "ERROR: the expected output $1.ref cannot be found."; print_status_and_exit "$1" "Failed (reference output not found)"; exit 1;}
+test -e ./$2 || { echo "ERROR: the datafile $2 cannot be found."; print_status_and_exit "$1" "Failed (datafile not found)"; exit 1;}
 
 # make sure that the reference output file is not empty (after removal of
 # comments and empty lines)
@@ -26,6 +38,7 @@ test -e ./$2 || { echo "ERROR: the datafile $2 cannot be found."; exit 1;}
     echo "ERROR: the reference output, $1.ref"
     echo "should contain more than comments and empty lines"
     echo
+    print_status_and_exit "$1" "Failed (no valid reference)"
     exit 1
 }
 
@@ -38,7 +51,10 @@ if [[ -n $DIFF ]]; then
     echo "ERROR: Outputs differ (diff available in $1.diff)"
     echo
     rm $1.tmp_ref
+    print_status_and_exit "$1" "Failed (see difference file)"
     exit 1
 fi
 
 rm $1.tmp_ref
+
+print_status_and_exit "$1" "Success"

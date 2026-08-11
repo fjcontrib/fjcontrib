3f7746f07c158d16348d7b9f7b3228440e294d18
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2026-08-11 09:59:06 +0100

    new-contrib now checks for existence of remote (with options of empty / v already populated remote)

--- scripts/new-contrib-from-template.sh
+++ scripts/new-contrib-from-template.sh
@@ -5,7 +5,7 @@
 #
 # create the structure of a new contrib
 
-. `dirname $0`/internal/common.sh
+. $(dirname $0)/internal/common.sh
 
 #------------------------------------------------------------------------
 # get the contrib name
@@ -24,6 +24,40 @@ contrib=${1%/}
 if [ -e $contrib ]; then
     echo "The $contrib contrib already exists. Please choose a different name"
     exit 1
+fi
+
+#------------------------------------------------------------------------
+# make sure a non-empty remote repository does not already use this name
+remote_repo_url="${git_repo_base_url%/}/${contrib}.git"
+remote_refs=""
+echo "Checking whether the remote repository exists: $remote_repo_url"
+echo
+if remote_refs=$(git ls-remote "$remote_repo_url" 2>&1); then
+    remote_exists=1
+elif [[ "$remote_refs" =~ [Rr]epository.*[Nn]ot[[:space:]]found|[Nn]ot[[:space:]]found ]]; then
+    # GitHub reports a repository that has not yet been created as an error.
+    remote_exists=0
+else
+    echo "Could not check whether the remote repository exists: $remote_repo_url"
+    echo "Aborting to avoid creating a contrib with an unverified repository name"
+    exit 1
+fi
+if [[ "$remote_exists" -eq 1 ]]; then
+    if [[ -n "$remote_refs" ]]; then
+        echo "The remote repository $remote_repo_url already exists and is not empty."
+        echo "In all likelihood the name has been reserved and you should choose a different name for your contrib."
+        if get_yesno_answer "Are you REALLY sure you want to create this contrib?"; then
+            echo "Aborting"
+            exit 1
+        fi
+    else
+        echo "The remote repository $remote_repo_url exists but is empty."   
+        echo "You should proceed with creation of $contrib ONLY if the remote was created for you"
+        if get_yesno_answer "Do you want to go ahead and create this contrib locally?"; then
+            echo "Aborting"
+            exit 1
+        fi
+    fi
 fi
 
 echo "Creating contrib "$1
@@ -66,8 +100,9 @@ echo "$contrib successfully created from Template. Rerun ./configure"
 echo "for it to be included in your builds."
 echo
 echo "Once you are ready to make it public, write to "
-echo "fastjet@projects.hepforge.org "
-echo "to ask for creation of the ${git_repo_base_url}/${contrib}.git repo "
+echo "fastjet@projects.hepforge.org to ask for creation of the "
+echo "         ${git_repo_base_url}/${contrib}.git repo "
+echo "indicating who should have write access to it."
 echo
 echo "You may then start to upload your contrib by running "
 echo
